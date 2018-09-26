d0ea62d2bd55d3d64da5d647f63db57fdace5a19
Alan Orth <alan.orth@gmail.com>
2018-09-26 22:23:24 +0300

    database.py: Use one line for psycopg2 imports

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ from config import DATABASE_NAME
 from config import DATABASE_USER
 from config import DATABASE_PASS
 from config import DATABASE_HOST
-import psycopg2
-import psycopg2.extras
+import psycopg2, psycopg2.extras
 
 def database_connection():
     connection = psycopg2.connect("dbname={} user={} password={} host='{}'".format(DATABASE_NAME, DATABASE_USER, DATABASE_PASS, DATABASE_HOST), cursor_factory=psycopg2.extras.DictCursor)
