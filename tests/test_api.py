92146fe4265c33f1c4174adab39ea815cdbc3179
Alan Orth <alan.orth@gmail.com>
2019-12-14 12:39:58 +0200

    tests/test_api.py: Format with black

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,57 +11,57 @@ def client():
 
 
 def test_get_docs(client):
-    '''Test requesting the documentation at the root.'''
+    """Test requesting the documentation at the root."""
 
-    response = client.simulate_get('/')
+    response = client.simulate_get("/")
 
     assert isinstance(response.content, bytes)
     assert response.status_code == 200
 
 
 def test_get_item(client):
-    '''Test requesting a single item.'''
+    """Test requesting a single item."""
 
-    response = client.simulate_get('/item/17')
+    response = client.simulate_get("/item/17")
     response_doc = json.loads(response.text)
 
-    assert isinstance(response_doc['downloads'], int)
-    assert isinstance(response_doc['id'], int)
-    assert isinstance(response_doc['views'], int)
+    assert isinstance(response_doc["downloads"], int)
+    assert isinstance(response_doc["id"], int)
+    assert isinstance(response_doc["views"], int)
     assert response.status_code == 200
 
 
 def test_get_missing_item(client):
-    '''Test requesting a single non-existing item.'''
+    """Test requesting a single non-existing item."""
 
-    response = client.simulate_get('/item/1')
+    response = client.simulate_get("/item/1")
 
     assert response.status_code == 404
 
 
 def test_get_items(client):
-    '''Test requesting 100 items.'''
+    """Test requesting 100 items."""
 
-    response = client.simulate_get('/items', query_string='limit=100')
+    response = client.simulate_get("/items", query_string="limit=100")
     response_doc = json.loads(response.text)
 
-    assert isinstance(response_doc['currentPage'], int)
-    assert isinstance(response_doc['totalPages'], int)
-    assert isinstance(response_doc['statistics'], list)
+    assert isinstance(response_doc["currentPage"], int)
+    assert isinstance(response_doc["totalPages"], int)
+    assert isinstance(response_doc["statistics"], list)
     assert response.status_code == 200
 
 
 def test_get_items_invalid_limit(client):
-    '''Test requesting 100 items with an invalid limit parameter.'''
+    """Test requesting 100 items with an invalid limit parameter."""
 
-    response = client.simulate_get('/items', query_string='limit=101')
+    response = client.simulate_get("/items", query_string="limit=101")
 
     assert response.status_code == 400
 
 
 def test_get_items_invalid_page(client):
-    '''Test requesting 100 items with an invalid page parameter.'''
+    """Test requesting 100 items with an invalid page parameter."""
 
-    response = client.simulate_get('/items', query_string='page=-1')
+    response = client.simulate_get("/items", query_string="page=-1")
 
     assert response.status_code == 400
